8fe9a3171cdaa009ee1b673fd8d0d1920008a5d8
Peter Friese <peter@peterfriese.de>
2021-09-06 12:18:14 +0200

    🚧 Seed starter to final
    Signed-off-by: Peter Friese <peter@peterfriese.de>

---
+++ Episode 5/final/ShipItToMe/ShipItToMe/EmailValidationForm.swift
@@ -0,0 +1,87 @@
+//
+//  EmailValidationForm.swift
+//  EmailValidationForm
+//
+//  Created by Peter Friese on 03.09.21.
+//
+
+import SwiftUI
+import Combine
+
+class EmailValidationViewModel: ObservableObject {
+  @Published var email = ""
+  @Published var emailConfirmation = ""
+  
+  @Published var isEmailValid = false
+  @Published var isEmailConfirmationValid = false
+  
+  @Published var errorMessage = ""
+  
+  @Published var isFormValid = false
+  
+  @Published var presentSuccessMessage = false
+  
+  init() {
+    let emailsMatchPublisher =
+    Publishers.CombineLatest($email, $emailConfirmation)
+      .map { $0 == $1 }
+      .share()
+    
+    emailsMatchPublisher
+      .map { $0 ? "" : "Emails don't match" }
+      .assign(to: &$errorMessage)
+    
+    emailsMatchPublisher
+      .combineLatest($isEmailValid, $isEmailConfirmationValid)
+      .map { $0 && $1 && $2 }
+      .assign(to: &$isFormValid)
+  }
+}
+
+struct EmailValidationForm: View {
+  @StateObject var viewModel = EmailValidationViewModel()
+  
+  var errorLabel: some View {
+    Text(viewModel.errorMessage)
+      .textCase(.none)
+      .foregroundColor(.red)
+  }
+  
+  var body: some View {
+    Form {
+      Section(header: errorLabel) {
+        TextInputField("Email address",
+                       text: $viewModel.email,
+                       isValid: $viewModel.isEmailValid)
+          .isMandatory()
+          .autocapitalization(.none)
+        
+        TextInputField("Confirm your email address",
+                       text: $viewModel.emailConfirmation,
+                       isValid: $viewModel.isEmailConfirmationValid)
+          .isMandatory()
+          .autocapitalization(.none)
+      }
+      Section {
+        Button("Submit") {
+          viewModel.presentSuccessMessage.toggle()
+        }
+        .disabled(!viewModel.isFormValid)
+      }
+    }
+    .navigationTitle("Email validation")
+    .alert("Success", isPresented: $viewModel.presentSuccessMessage) {
+      Button("OK") { }
+    } message: {
+      Text("You entered: \(viewModel.email)")
+    }
+  }
+}
+
+struct EmailValidationForm_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationView {
+      EmailValidationForm()
+    }
+  }
+}
